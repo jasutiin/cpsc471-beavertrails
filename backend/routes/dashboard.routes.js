b8c39c874166a2a470e2156f27e1c1151766e1ee
justine <justinemangaliman@gmail.com>
2025-04-22 15:35:25 -0600

    feat(backend): api endpoints for getting info in dashboard

--- backend/routes/dashboard.routes.js
+++ backend/routes/dashboard.routes.js
@@ -1,11 +1,19 @@
 import { Router } from 'express';
-import { getCompanyFlights } from '../controllers/dashboard.controller.js';
+import {
+  getCompanyFlights,
+  getCompanyBuses,
+  getCompanyHotelRooms,
+  getCompanyActivities,
+} from '../controllers/dashboard.controller.js';
 
 const dashboardRouter = Router();
 
-dashboardRouter.get(
-  '/company/dashboard/flights/:company_id',
-  getCompanyFlights
-);
+dashboardRouter.get('/company/:company_id/flights', getCompanyFlights);
+
+dashboardRouter.get('/company/:company_id/buses', getCompanyBuses);
+
+dashboardRouter.get('/company/:company_id/hotel-rooms', getCompanyHotelRooms);
+
+dashboardRouter.get('/company/:company_id/activities', getCompanyActivities);
 
 export default dashboardRouter;
